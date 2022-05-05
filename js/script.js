25f4c7a0dee291f97d4b55474286feaaf21e2b2e
Nicola Sarli <you@example.com>
2022-05-05 16:48:44 +0200

    bonus done: image printed in HTML with Vue using data

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ const app = new Vue(
     {
         el: '#root',
         data: {
-            message: 'Stampare a schermo un messaggio dentro h1, utilizzando i data',
+            message: "Stampare a schermo un messaggio dentro h1, utilizzando i data(l'ho stampato di proposito come la consegna dell'esercizio).",
+            image: '../vue-hello/img/2020552.jpg'
         }
     }
 );
