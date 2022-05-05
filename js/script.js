40bf26dcb7f9bbcdfc5724131c1caef613fc3675
Nicola Sarli <you@example.com>
2022-05-05 16:38:07 +0200

    Scaffolding + base exercise done: printed in HTML a message from Vue using data

---
+++ js/script.js
@@ -0,0 +1,10 @@
+Vue.config.devtools;
+
+const app = new Vue(
+    {
+        el: '#root',
+        data: {
+            message: 'Stampare a schermo un messaggio dentro h1, utilizzando i data',
+        }
+    }
+);
